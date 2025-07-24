f6d68fe4cf3371eacb676327b1e2353e3860a6dd
Freitas1887 <janderson.freitas.eb@gmail.com>
2025-07-24 05:04:46 -0300

    Game Loop

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable {
 
     public static JFrame frame;
+    private Thread thread;
+    private boolean isRunning = true;
     private final int WIDTH = 160;
     private final int HEIGHT = 120;
     private final int SCALE = 4;
 
+    private BufferedImage image;
+
     public Game(){
-        this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        initFrame();
+        image = new BufferedImage(160,120, BufferedImage.TYPE_INT_ARGB);
+    }
+    public void initFrame(){
         frame = new JFrame("Game #1");
         frame.add(this);
         frame.setResizable(false);
@@ -19,12 +29,74 @@ public class Game extends Canvas implements Runnable {
         frame.setVisible(true);
     }
 
+    public synchronized void start(){
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+
+    }
+
+    public  synchronized void stop(){
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String args[]){
     Game game = new Game();
+    game.start();
+    }
+
+    public void tick(){
+
+    }
+    public void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = image.getGraphics();
+        g.setColor(new Color(250,216,168));
+        g.fillRect(0,0,WIDTH,HEIGHT);
+
+        g.setColor(Color.CYAN);
+        g.fillRect(20,20,20,20);
+        g = bs.getDrawGraphics();
+        g.drawImage(image, 0 ,0, WIDTH*SCALE, HEIGHT*SCALE, null);
+        bs.show();
+
     }
 
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        int frames = 0;
+        double timer = System.currentTimeMillis();
 
+        while(isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1) {
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
+
+            if (System.currentTimeMillis() - timer >= 1000){
+                System.out.println("FPS: " + frames);
+                frames = 0;
+                timer += 1000;
+            }
+        }
+        stop();
     }
 }
